f888ae6df0db9862d75d2dc118218749e16fd092
CashlockH <ilyashesenli48@gmail.com>
2024-04-09 07:22:02 +0000

    function's name is corrected for recursion

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -10,9 +10,9 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree != NULL && *func != NULL)
 	{
 		if (tree->left != NULL)
-			binary_tree_inorder(tree->left, func);
+			binary_tree_postorder(tree->left, func);
 		if (tree->right != NULL)
-			binary_tree_inorder(tree->right, func);
+			binary_tree_postorder(tree->right, func);
 		(*func)(tree->n);
 	}
 }
