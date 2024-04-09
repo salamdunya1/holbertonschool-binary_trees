f9e69ed9c98c8961fa198d5febed063cbcaef11f
CashlockH <ilyashesenli48@gmail.com>
2024-04-09 07:00:42 +0000

    preorder traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,8 +11,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		(*func)(tree->n);
 		if (tree->left != NULL)
-			binary_tree_preorder(tree->left, &print_num);
+			binary_tree_preorder(tree->left, func);
 		if (tree->right != NULL)
-			binary_tree_preorder(tree->right, &print_num);
+			binary_tree_preorder(tree->right, func);
 	}
 }
