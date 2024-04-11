f8a9a1b6785fe40ec965730be6d050315560eba6
Salam Dunya <burmurdack@email.com>
2024-04-11 16:59:25 +0000

    function that counts the nodes with at least 1 child in a binary tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - counts the nods with at least 1 child in a binary tree
+ * binary_tree_nodes - counts the nodss with at least 1 child in a binary tree
  * @tree: pointer to the root node of the tree to count the number of nodes
  * Return: 0 if tree = NULL
  */
